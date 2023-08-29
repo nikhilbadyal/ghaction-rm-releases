0e90b424cf309747f3895d8f07b867c3c429201c
Nikhil Badyal <nikhill773384@gmail.com>
2023-08-29 18:25:18 +0530

    ✅ Updated TC

--- __test__/utils.test.ts
+++ __test__/utils.test.ts
@@ -1,14 +1,5 @@
 /* eslint-disable @typescript-eslint/no-magic-numbers */
 import { describe, expect, it } from '@jest/globals'
-import {
-  deleteRelease,
-  deleteTag,
-  getMyOctokit,
-  getReleases,
-  Release,
-  rmReleases
-} from '../src/github'
-import { debug } from '@actions/core'
 import { getInputs } from '../src/utils'
 
 // See: https://github.com/actions/toolkit/blob/master/packages/core/src/core.ts#L67
@@ -19,10 +10,10 @@ function getInputName(name: string): string {
 function setInput(name: string, value: string): void {
   process.env[getInputName(name)] = value
 }
-describe('github', () => {
+describe('get inputs', () => {
   it('should getInputs', async function () {
-    await setInput('GITHUB_TOKEN', 'mytoken')
-    await setInput('RELEASE_PATTERN', 'pattern*')
+    setInput('GITHUB_TOKEN', 'mytoken')
+    setInput('RELEASE_PATTERN', 'pattern*')
     let inputs = getInputs()
     expect(inputs.GITHUB_TOKEN).toEqual('mytoken')
     expect(inputs.RELEASE_PATTERN).toEqual('pattern*')
